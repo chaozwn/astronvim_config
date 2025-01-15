349bd35624cc3f277ed0b0e702dcde1272bc2f61
jayce.zhao <326747337@qq.com>
2025-01-15 10:14:33 +0800

    feat: update dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -31,7 +31,7 @@ local choose_dap_element = function(callback)
   }, { prompt = "Select Dap Layout: ", default = "repl&console" }, function(select)
     if not select then return end
     if is_dap_window_open() then close_all_window() end
-    if select == "console&scopes" then
+    if select == "console|scopes" then
       require("dapui").open { layout = 1, reset = true }
     elseif select == "console" then
       require("dapui").open { layout = 2, reset = true }
@@ -45,7 +45,7 @@ local choose_dap_element = function(callback)
       require("dapui").open { layout = 6, reset = true }
     elseif select == "scopes" then
       require("dapui").open { layout = 7, reset = true }
-    elseif select == "repl&console" then
+    elseif select == "repl|console" then
       require("dapui").open { layout = 9, reset = true }
     else
       require("dapui").open { layout = 8, reset = true }
