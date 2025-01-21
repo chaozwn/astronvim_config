114fe106ee23779bcb9a637702b40d9743decc2b
jayce.zhao <326747337@qq.com>
2025-01-22 00:16:25 +0800

    feat: update

--- lua/plugins/mini-diff.lua
+++ lua/plugins/mini-diff.lua
@@ -1,7 +1,7 @@
 ---@type LazySpec
 return {
   "echasnovski/mini.diff",
-  event = "VeryLazy",
+  event = "User AstroGitFile",
   opts = function()
     local sign = require("astroui").get_icon "GitSign"
     return {
